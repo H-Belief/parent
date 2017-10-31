b1c42d1494e801420de1329d02623283ad0f2bb3
huguoyu <hu.guoyu@microcardio.com>
2017-10-31 14:02:29 +0800

    设置首页

---
+++ src/main/java/com/hgy/configuration/WebConfig.java
@@ -0,0 +1,23 @@
+package com.hgy.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.util.ResourceUtils;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * Created by huguoyu on 2017/10/31.
+ */
+@Configuration
+@EnableWebMvc
+@ComponentScan
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        //将所有/static/** 访问都映射到classpath:/static/ 目录下
+        registry.addResourceHandler("/static/**").addResourceLocations(ResourceUtils.CLASSPATH_URL_PREFIX+"/static/");
+    }
+}
